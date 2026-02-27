72fe49dcddb4091ce1e96f799d18d9c596a4d0c0
3Matthew3 <64413289+3Matthew3@users.noreply.github.com>
2026-02-27 10:09:57 +0100

    Update page.tsx

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,14 +1,19 @@
 import { RealHomePage } from "@/components/real-home-page"
+import { AccessGate } from "@/components/access-gate"
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
-    title: "RoadRulez App",
-    robots: {
-        index: false,
-        follow: false,
-    },
+  title: "RoadRulez App",
+  robots: {
+    index: false,
+    follow: false,
+  },
 }
 
-export default function BetaPage({ params }: { params: { lang: string } }) {
-    return <RealHomePage lang={params.lang} />
+export default function Home({ params }: { params: { lang: string } }) {
+  return (
+    <AccessGate>
+      <RealHomePage lang={params.lang} />
+    </AccessGate>
+  )
 }
